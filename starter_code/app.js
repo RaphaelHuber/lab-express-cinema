086b53bcf919cba82203d7fa86300d3c12c26d06
Raphael Huber <raphael.ehuber@gmail.com>
2019-01-31 22:14:39 -0200

    Until Iteration 2

--- starter_code/app.js
+++ starter_code/app.js
@@ -37,22 +37,16 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
-
 const index = require('./routes/index');
 app.use('/', index);
-
 
 module.exports = app;
